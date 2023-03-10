d40e14c12bdbe4232debe5bc33360e406bbe0b09
shureed100 <shureedshazzad534@gmail.com>
2023-03-10 23:52:30 +0600

    First Commit

---
+++ src/main/java/com/example/busticketbd/new_class.java
@@ -0,0 +1,28 @@
+package com.example.busticketbd;
+
+public class newclass {
+    private String s;
+    int i;
+
+    public newclass(String s, int i) {
+        this.s = s;
+        this.i = i;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+}
+
